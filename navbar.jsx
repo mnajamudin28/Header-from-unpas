305d468007a6ea3afddde83cb9958594ec34cde4
Moe <43162722+mnajamudin28@users.noreply.github.com>
2022-08-11 21:13:51 +0700

    Create navbar.jsx

---
+++ navbar.jsx
@@ -0,0 +1,36 @@
+// import React from 'react'
+// import '../../css/Home.css'
+// import Logo from '../../img/MN.png'
+// import { Link } from "react-router-dom";
+
+
+
+// const Navbar = () => {
+//    const menuToggle = document.querySelector('.menu-toggle input');
+//    const nav = document.querySelector('nav ul')
+
+//    menuToggle.addEventListener('click', function () {
+//       nav.classList.toggle('slide')
+//    })
+//   return (
+//     <div>
+//       <nav>
+//          <div className='nav-logo'> <img src={Logo} alt="" /></div>
+//          <ul className='slide'>
+//               <li> <Link to="/CV" className='txt-nav'>CV</Link> </li>
+//               <li> <Link to="/Portofolio" className='txt-nav'>Portofolio</Link></li>
+//               <li> <Link to="/Contact" className='txt-nav'>Contact</Link> </li>
+//          </ul>
+
+//          <div className="menu-toggle">
+//             <input type="checkbox" />
+//             <span></span>
+//             <span></span>
+//             <span></span>
+//          </div>
+//       </nav>
+//     </div>
+//   )
+// }
+
+// export default Navbar
